1c885f344c821e07cfa7b700aa1891f4677cf739
Mathu025 <mathualex72@gmail.com>
2025-08-27 16:51:16 +0300

    Configure Alembic env.py to use SQLAlchemy Base metadata

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,9 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from travel_booking_system.database import Base
+from travel_booking_system import models
+
 
 from alembic import context
 
@@ -18,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
