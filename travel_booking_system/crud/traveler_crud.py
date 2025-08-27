91a94ba00ca3ecac29b86bd60f8078353e928b16
Mathu025 <mathualex72@gmail.com>
2025-08-27 18:20:28 +0300

    Add crud/ to handle all database operations

---
+++ travel_booking_system/crud/traveler_crud.py
@@ -0,0 +1,40 @@
+from database import SessionLocal
+from models import Traveler
+
+def add_traveler(name, email, phone, passport_number):
+    session = SessionLocal()
+    traveler = Traveler(
+        name=name,
+        email=email,
+        phone=phone,
+        passport_number=passport_number
+    )
+    session.add(traveler)
+    session.commit()
+    session.refresh(traveler)
+    session.close()
+    return traveler
+
+def list_travelers():
+    session = SessionLocal()
+    travelers = session.query(Traveler).all()
+    session.close()
+    return travelers
+
+def get_traveler(traveler_id):
+    session = SessionLocal()
+    traveler = session.query(Traveler).filter_by(id=traveler_id).first()
+    session.close()
+    return traveler
+
+def delete_traveler(traveler_id):
+    session = SessionLocal()
+    traveler = session.query(Traveler).filter_by(id=traveler_id).first()
+    if traveler:
+        session.delete(traveler)
+        session.commit()
+        result = True
+    else:
+        result = False
+    session.close()
+    return result
